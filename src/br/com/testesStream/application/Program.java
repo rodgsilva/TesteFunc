aad92525242bbee045ea5d1ba167b59e58a18abc
rgomsilv <rgomsilv@SAO-D1GW9R2.usersad.everis.int>
2019-05-22 11:15:25 -0300

    Alinhado

--- src/br/com/testesStream/application/Program.java
+++ src/br/com/testesStream/application/Program.java
@@ -10,7 +10,7 @@ import br.com.testesStream.entity.Paciente;
 public class Program {
 
 	public static void main(String[] args) {
-		
+
 		List<Paciente> list = new ArrayList<>();
 		list.add(new Paciente("Rodrigo", "Masc", 100.00, 35, 1.75));
 		list.add(new Paciente("Maria", "Fem", 15.00, 5, 1.10));
@@ -20,57 +20,38 @@ public class Program {
 		list.add(new Paciente("Gabi", "Fem", 85.00, 60, 1.65));
 
 		int sum = list.size();
-		System.out.println("Quantidade de paciente = "+sum);
-		String sexo ="Masc";
-		
-		Comparator<String> comp =(s1, s2) -> s1.toUpperCase().compareTo(s2.toUpperCase());
-		List<String> homeMediaIdade = new ArrayList();
-		
-		 homeMediaIdade = list.stream()
-									  .filter(p -> p.getSexo().equals(sexo) )
-									  .map(p -> p.getNome()).sorted(comp)
-									  .collect(Collectors.toList());
-									  
-		System.out.println("Homes = "+ homeMediaIdade);
-		
-		double avg = list.stream()
-					.filter(p -> p.getSexo().equals(sexo) )
-					.map(p -> p.getIdade())
-					.reduce((int) 0.0, (x,y)-> x + y) / list.size();
-		System.out.println("Idade média dos homens = "+ avg);
-		
-		List<String> qtdMulher = list.stream()
-						.filter(p -> p.getAltura() > 1.60 )
-						.filter(p -> p.getAltura() < 1.70 )
-						.filter(p -> p.getPeso() > 70 )
-						.filter(p -> p.getSexo().equals("Fem"))
-						.map(p -> p.getNome()).sorted(comp)
-						.collect(Collectors.toList());
-		
-		System.out.println("Nome= "+qtdMulher +" Quantidade de mulher 1.60 e 1.70 / 70kg = "+ qtdMulher.size());
-		List<String> qtdPessoa18a25 = list.stream()	
-								.filter(p -> p.getIdade() > 18)
-								.filter(p -> p.getIdade() < 25)
-								.map(p -> p.getNome()).sorted(comp)
-								.collect(Collectors.toList());
-		System.out.println("Qtd pessoa entre 18 e 25 = "+ qtdPessoa18a25.size());
+		System.out.println("Quantidade de paciente = " + sum);
+		String sexo = "Masc";
 
-		Comparator<Paciente> comp2 = Comparator.comparing( Paciente::getIdade );
+		Comparator<String> comp = (s1, s2) -> s1.toUpperCase().compareTo(s2.toUpperCase());
+		List<String> homeMediaIdade = new ArrayList();
+
+		homeMediaIdade = list.stream().filter(p -> p.getSexo().equals(sexo)).map(p -> p.getNome()).sorted(comp)
+				.collect(Collectors.toList());
+		System.out.println("Homes = " + homeMediaIdade);
+
+		double avg = list.stream().filter(p -> p.getSexo().equals(sexo)).map(p -> p.getIdade()).reduce((int) 0.0,
+				(x, y) -> x + y) / list.size();
+		System.out.println("Idade média dos homens = " + avg);
+
+		List<String> qtdMulher = list.stream().filter(p -> p.getAltura() > 1.60).filter(p -> p.getAltura() < 1.70)
+				.filter(p -> p.getPeso() > 70).filter(p -> p.getSexo().equals("Fem")).map(p -> p.getNome()).sorted(comp)
+				.collect(Collectors.toList());
+		System.out.println("Nome= " + qtdMulher + " Quantidade de mulher 1.60 e 1.70 / 70kg = " + qtdMulher.size());
 		
-		Paciente pasMaisVelho = list.stream()
-									.filter(p -> p.getSexo().equals("Fem"))
-									.max(comp2)
-									.get();
-		System.out.println("Pasiente mais velha = "+pasMaisVelho.getNome());
 		
-Comparator<Paciente> comp3 = Comparator.comparing( Paciente::getIdade );
-		
-		Paciente pasMaisBaixa = list.stream()
-									.filter(p -> p.getSexo().equals("Fem"))
-									.min(comp3)
-									.get();
-		System.out.println("Pasiente mais velha = "+pasMaisBaixa.getNome());
-		
+		List<String> qtdPessoa18a25 = list.stream().filter(p -> p.getIdade() > 18).filter(p -> p.getIdade() < 25)
+				.map(p -> p.getNome()).sorted(comp).collect(Collectors.toList());
+		System.out.println("Qtd pessoa entre 18 e 25 = " + qtdPessoa18a25.size());
+
+		Comparator<Paciente> comp2 = Comparator.comparing(Paciente::getIdade);
+		Paciente pasMaisVelho = list.stream().filter(p -> p.getSexo().equals("Fem")).max(comp2).get();
+		System.out.println("Pasiente mais velha = " + pasMaisVelho.getNome());
+
+		Comparator<Paciente> comp3 = Comparator.comparing(Paciente::getIdade);
+		Paciente pasMaisBaixa = list.stream().filter(p -> p.getSexo().equals("Fem")).min(comp3).get();
+		System.out.println("Pasiente mais velha = " + pasMaisBaixa.getNome());
+
 	}
 
 }
